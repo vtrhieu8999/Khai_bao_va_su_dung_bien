04e878c5ccf1827978187126ad0a8ba676894a3d
Vuong_Trung_Hieu <v.hieu1998@gmail.com>
2020-08-11 09:09:49 +0700

    Khai bao va su dung bien

---
+++ Boi_so.js
@@ -0,0 +1,10 @@
+let inputA = prompt("Nhap a");
+let inputB = prompt("Nhap b");
+let a = parseInt(inputA);
+let b = parseInt(inputB);
+if (a%b==0){
+    alert("A la boi so cua B");
+}
+else {
+    alert("A khong phai boi cua B");
+}
